66f84e4ffe29bd0de2555d727572cf82df79a6ce
wooseob <dksdntjq3285@naver.com>
2024-08-05 22:53:19 +0900

    [안우섭] 연습

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,83 +1,125 @@
-import './App.css'
-import React, { useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Search from './pages/Search';
-import Info from './pages/Info';
-import Description from './pages/Description';
-import Review from './pages/Review';
-import NewReview from './pages/NewReview';
-import EditReview from './pages/EditReview';
-import MyPlace from './pages/MyPlace';
-import MyPlaceSmoking from './pages/MyPlaceSmoking';
-import SelectMyPlaceToilet from './pages/SelectMyPlaceToilet';
-import SelectMyPlaceSmoking from './pages/SelectMyPlaceSmoking';
-import NewMyPlaceToilet from './pages/NewMyPlaceToilet';
-import NewMyPlaceSmoking from './pages/NewMyPlaceSmoking';
-import EditMyPlaceToilet from './pages/EditMyPlaceToilet';
-import EditMyPlaceSmoking from './pages/EditMyPlaceSmoking';
-import initialToilets from './util/initialToilets';
-import initialSmokings from './util/initialSmokings';
-import MyPage from './pages/MyPage';
+import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Info from "./pages/Info";
+import Description from "./pages/Description";
+import Review from "./pages/Review";
+import NewReview from "./pages/NewReview";
+import EditReview from "./pages/EditReview";
+import MyPlace from "./pages/MyPlace";
+import MyPlaceSmoking from "./pages/MyPlaceSmoking";
+import SelectMyPlaceToilet from "./pages/SelectMyPlaceToilet";
+import SelectMyPlaceSmoking from "./pages/SelectMyPlaceSmoking";
+import NewMyPlaceToilet from "./pages/NewMyPlaceToilet";
+import NewMyPlaceSmoking from "./pages/NewMyPlaceSmoking";
+import EditMyPlaceToilet from "./pages/EditMyPlaceToilet";
+import EditMyPlaceSmoking from "./pages/EditMyPlaceSmoking";
+import initialToilets from "./util/initialToilets";
+import initialSmokings from "./util/initialSmokings";
+import MyPage from "./pages/MyPage";
 
 function App() {
+  const [toilets, setToilets] = useState(initialToilets);
+  const [smokings, setSmokings] = useState(initialSmokings);
+  const navigate = useNavigate();
 
-    const addToilet = (newToilet) => {
-        setToilets(prevToilets => [...prevToilets, newToilet]);
-        navigate('/myplace');
-    };
+  const addToilet = (newToilet) => {
+    setToilets((prevToilets) => [...prevToilets, newToilet]);
+    navigate("/myplace");
+  };
 
-    const addSmoking = (newSmoking) => {
-        setSmokings(prevSmokings => [...prevSmokings, newSmoking]);
-        navigate('/myplacesmoking');
-    };
+  const addSmoking = (newSmoking) => {
+    setSmokings((prevSmokings) => [...prevSmokings, newSmoking]);
+    navigate("/myplacesmoking");
+  };
 
-    const updateToilet = (updatedToilet) => {
-        setToilets(prevToilets =>
-            prevToilets.map(toilet =>
-                toilet.id === updatedToilet.id ? updatedToilet : toilet
-            )
-        );
-    };
-
-    const updateSmoking = (updatedSmoking) => {
-        setSmokings(prevSmokings =>
-            prevSmokings.map(smoking =>
-                smoking.id === updatedSmoking.id ? updatedSmoking : smoking
-            )
-        );
-    };
-
-    const deleteToilet = (id) => {
-        setToilets(prevToilets => prevToilets.filter(toilet => toilet.id !== id));
-    };
-
-    const deleteSmoking = (id) => {
-        setSmokings(prevSmokings => prevSmokings.filter(smoking => smoking.id !== id));
-    };
-
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/search' element={<Search />} />
-                <Route path='/info' element={<Info />} />
-                <Route path='/description' element={<Description />} />
-                <Route path='/review' element={<Review />} />
-                <Route path='/newreview' element={<NewReview />} />
-                <Route path='/editreview' element={<EditReview />} />
-                <Route path='/myplace' element={<MyPlace toilets={toilets} onDeleteToilet={deleteToilet} />} />
-                <Route path='/myplacesmoking' element={<MyPlaceSmoking smokings={smokings} onDeleteSmoking={deleteSmoking} />} />
-                <Route path='/selectmyplacetoilet' element={<SelectMyPlaceToilet />} />
-                <Route path='/selectmyplacesmoking' element={<SelectMyPlaceSmoking />} />
-                <Route path='/newmyplacetoilet' element={<NewMyPlaceToilet addToilet={addToilet} />} />
-                <Route path='/newmyplacesmoking' element={<NewMyPlaceSmoking addSmoking={addSmoking} />} />
-                <Route path='/editmyplacetoilet/:id' element={<EditMyPlaceToilet toilets={toilets} onUpdateToilet={updateToilet} />} />
-                <Route path='/editmyplacesmoking/:id' element={<EditMyPlaceSmoking smokings={smokings} onUpdateSmoking={updateSmoking} />} />
-                <Route path='/mypage' element={<MyPage />} />
-            </Routes>
-        </>
+  const updateToilet = (updatedToilet) => {
+    setToilets((prevToilets) =>
+      prevToilets.map((toilet) =>
+        toilet.id === updatedToilet.id ? updatedToilet : toilet
+      )
     );
+  };
+
+  const updateSmoking = (updatedSmoking) => {
+    setSmokings((prevSmokings) =>
+      prevSmokings.map((smoking) =>
+        smoking.id === updatedSmoking.id ? updatedSmoking : smoking
+      )
+    );
+  };
+
+  const deleteToilet = (id) => {
+    setToilets((prevToilets) =>
+      prevToilets.filter((toilet) => toilet.id !== id)
+    );
+  };
+
+  const deleteSmoking = (id) => {
+    setSmokings((prevSmokings) =>
+      prevSmokings.filter((smoking) => smoking.id !== id)
+    );
+  };
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/description" element={<Description />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/newreview" element={<NewReview />} />
+        <Route path="/editreview" element={<EditReview />} />
+        <Route
+          path="/myplace"
+          element={<MyPlace toilets={toilets} onDeleteToilet={deleteToilet} />}
+        />
+        <Route
+          path="/myplacesmoking"
+          element={
+            <MyPlaceSmoking
+              smokings={smokings}
+              onDeleteSmoking={deleteSmoking}
+            />
+          }
+        />
+        <Route path="/selectmyplacetoilet" element={<SelectMyPlaceToilet />} />
+        <Route
+          path="/selectmyplacesmoking"
+          element={<SelectMyPlaceSmoking />}
+        />
+        <Route
+          path="/newmyplacetoilet"
+          element={<NewMyPlaceToilet addToilet={addToilet} />}
+        />
+        <Route
+          path="/newmyplacesmoking"
+          element={<NewMyPlaceSmoking addSmoking={addSmoking} />}
+        />
+        <Route
+          path="/editmyplacetoilet/:id"
+          element={
+            <EditMyPlaceToilet
+              toilets={toilets}
+              onUpdateToilet={updateToilet}
+            />
+          }
+        />
+        <Route
+          path="/editmyplacesmoking/:id"
+          element={
+            <EditMyPlaceSmoking
+              smokings={smokings}
+              onUpdateSmoking={updateSmoking}
+            />
+          }
+        />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- front-end/src/pages/MyPlace.jsx
+++ front-end/src/pages/MyPlace.jsx
@@ -1,89 +1,128 @@
-import React, { useState } from 'react';
-import '../styles/MyPlace.css';
-import CustomButton from '../components/CustomButton'
-import Header from '../components/Header';
-import Modal from '../components/Modal';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "../styles/MyPlace.css";
+import CustomButton from "../components/CustomButton";
+import Header from "../components/Header";
+import Modal from "../components/Modal";
+import { useNavigate } from "react-router-dom";
 
 const MyPlace = ({ toilets, onDeleteToilet }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedToiletId, setSelectedToiletId] = useState(null);
-    const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedToiletId, setSelectedToiletId] = useState(null);
+  const navigate = useNavigate();
 
-    const handleEditClick = (id) => {
-        navigate(`/editmyplacetoilet/${id}`);
-    };
+  const handleEditClick = (id) => {
+    navigate(`/editmyplacetoilet/${id}`);
+  };
 
-    const handleDeleteClick = (id) => {
-        setSelectedToiletId(id);
-        setIsModalOpen(true);
-    };
+  const handleDeleteClick = (id) => {
+    setSelectedToiletId(id);
+    setIsModalOpen(true);
+  };
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-        setSelectedToiletId(null);
-    };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedToiletId(null);
+  };
 
-    const handleConfirmDelete = () => {
-        onDeleteToilet(selectedToiletId);
-        handleCloseModal();
-    };
+  const handleConfirmDelete = () => {
+    onDeleteToilet(selectedToiletId);
+    handleCloseModal();
+  };
 
-const MyPlace = () => {
-    const nav = useNavigate()
-    
-    return (
-        <div className='MyPlace'>
-            <Header
-                title={'내 장소 목록'}
-                leftChild={<CustomButton onClick={() => nav(-1)} text={<svg xmlns="http://www.w3.org/2000/svg" width="10" height="20" viewBox="0 0 13 24" fill="none">
-                    <path d="M10.752 23.994L0.87897 14.121C0.316556 13.5584 0.000610352 12.7955 0.000610352 12C0.000610352 11.2045 0.316556 10.4415 0.87897 9.87896L10.746 0.0119629L12.16 1.42596L2.29297 11.293C2.1055 11.4805 2.00018 11.7348 2.00018 12C2.00018 12.2651 2.1055 12.5194 2.29297 12.707L12.166 22.58L10.752 23.994Z" fill="black"/>
-                    </svg>} />}
-                rightChild={<CustomButton text={'추가'} />}
-            />
+  return (
+    <div className="MyPlace">
+      <Header
+        title="내 장소 목록"
+        leftChild={
+          <CustomButton
+            onClick={() => navigate(-1)}
+            text={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                height="20"
+                viewBox="0 0 13 24"
+                fill="none"
+              >
+                <path
+                  d="M10.752 23.994L0.87897 14.121C0.316556 13.5584 0.000610352 12.7955 0.000610352 12C0.000610352 11.2045 0.316556 10.4415 0.87897 9.87896L10.746 0.0119629L12.16 1.42596L2.29297 11.293C2.1055 11.4805 2.00018 11.7348 2.00018 12C2.00018 12.2651 2.1055 12.5194 2.29297 12.707L12.166 22.58L10.752 23.994Z"
+                  fill="black"
+                />
+              </svg>
+            }
+          />
+        }
+        rightChild={
+          <CustomButton
+            text="추가"
+            onClick={() => navigate("/newmyplacetoilet")}
+          />
+        }
+      />
 
-            <div className='choosePlace'>
-                <p>화장실</p>
-                <p className='Mysmoking'>흡연구역</p>
+      <div className="choosePlace">
+        <p className="Mytoilet" onClick={() => navigate("/myplace")}>
+          화장실
+        </p>
+        <p className="Mysmoking" onClick={() => navigate("/myplacesmoking")}>
+          흡연구역
+        </p>
+      </div>
+      <div className="nav-bar">
+        <svg
+          width="478"
+          height="3"
+          viewBox="0 0 478 3"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line y1="1" x2="478.004" y2="1" stroke="#E9E9EB" strokeWidth="2" />
+          <path
+            d="M97 2L129 2"
+            stroke="black"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+        </svg>
+      </div>
+
+      <div className="sort-bar">
+        <p>등록순</p>
+      </div>
+
+      <hr className="bar" />
+
+      {toilets.length === 0 ? (
+        <p>저장된 화장실이 없습니다.</p>
+      ) : (
+        toilets.map((toilet) => (
+          <div key={toilet.id} className="place">
+            <p>{toilet.name}</p>
+            <p>{toilet.location}</p>
+            <p>{toilet.detailedLocation}</p>
+            <p>{toilet.description}</p>
+            <div className="buttons">
+              <CustomButton
+                text="수정"
+                onClick={() => handleEditClick(toilet.id)}
+              />
+              <CustomButton
+                text="삭제"
+                onClick={() => handleDeleteClick(toilet.id)}
+              />
             </div>
-            <div className='nav-bar'>
-                <svg width="478" height="2" viewBox="0 0 478 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line y1="1" x2="478.004" y2="1" stroke="#E9E9EB" strokeWidth="2"/>
-                <path d="M102.002 1L134.002 1" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                </svg>
-            </div>
+            <hr className="bar" />
+          </div>
+        ))
+      )}
 
-            <div className='sort-bar'>
-                <p>등록순</p>
-            </div>
-
-            <hr className='bar'/>
-
-            {toilets.length === 0 ? (
-                <p>저장된 화장실이 없습니다.</p>
-            ) : (
-                toilets.map(toilet => (
-                    <div key={toilet.id} className='place'>
-                        <p>{toilet.name}</p>
-                        <p>{toilet.location}</p>
-                        <p>{toilet.detailedLocation}</p>
-                        <p>{toilet.description}</p>
-                        <div className='buttons'>
-                            <CustomButton text='수정' onClick={() => handleEditClick(toilet.id)} />
-                            <CustomButton text='삭제' onClick={() => handleDeleteClick(toilet.id)} />
-                        </div>
-                        <hr className='bar'/>
-                    </div>
-                ))
-            )}
-            
-            <Modal 
-                isOpen={isModalOpen} 
-                onClose={handleCloseModal} 
-                onConfirm={handleConfirmDelete} 
-            />
-        </div>
-    )
-}
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onConfirm={handleConfirmDelete}
+      />
+    </div>
+  );
+};
 
 export default MyPlace;
