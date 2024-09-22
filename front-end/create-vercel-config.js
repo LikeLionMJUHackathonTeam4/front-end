a0b3f6ddcd06333e7d48642f7ddad1d2f4c37fd5
MoonInbae <mib010213@gmail.com>
2024-09-23 04:51:18 +0900

    [Chore] 배포 설정

--- front-end/create-vercel-config.js
+++ front-end/create-vercel-config.js
@@ -1,5 +1,5 @@
 // create-vercel-config.js
-const fs = require('fs');
+import { writeFileSync } from 'fs';
 
 // 환경 변수 값 가져오기
 const apiUrl = process.env.VITE_BE_ENDPOINT;
@@ -7,7 +7,8 @@ const apiUrl = process.env.VITE_BE_ENDPOINT;
 if (!apiUrl) {
     console.error('환경 변수 VITE_BE_ENDPOINT가 설정되지 않았습니다.');
     process.exit(1); // 오류 발생 시 스크립트 종료
-  }
+}
+console.log('VITE_BE_ENDPOINT:', apiUrl);
 
 // vercel.json 파일 생성
 const vercelConfig = {
@@ -24,6 +25,10 @@ const vercelConfig = {
 };
 
 // vercel.json 파일을 생성하고 저장
-fs.writeFileSync('vercel.json', JSON.stringify(vercelConfig, null, 2));
-
-console.log('vercel.json 파일이 성공적으로 생성되었습니다.');
+try {
+    writeFileSync('vercel.json', JSON.stringify(vercelConfig, null, 2));
+    console.log('vercel.json 파일이 성공적으로 생성되었습니다.');
+  } catch (error) {
+    console.error('vercel.json 파일 생성 중 오류 발생:', error);
+    process.exit(1); // 오류 발생 시 스크립트 종료
+  }
