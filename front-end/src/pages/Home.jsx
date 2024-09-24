803fc6520773a3b9b90a840a63f9691fa96f96a9
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-09-24 21:20:43 +0900

    [Fix] vercel 설정 수정

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -19,9 +19,8 @@ const Home = () => {
     // 화장실 데이터 불러오기 함수
     const fetchToiletData = async () => {
         try {
-            const response = await axios.get(`/api/toilets/all`); // 실제 API 경로로 변경
+            const response = await axios.get(`${baseUrl}/toilets/all`); // 실제 API 경로로 변경
             if (response.data && response.data.code === 200) {
-                console.log('응답 데이터:', response.data);
                 console.error('화장실 데이터 출력:', response.data.data);
                 setToilets(response.data.data);
                 if (mapRef.current) {
